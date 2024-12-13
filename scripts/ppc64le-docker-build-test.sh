4c86d7dac2240202793024767bb8c8c4a69e7c93
samofoz <39989098+samofoz@users.noreply.github.com>
2024-12-13 15:59:10 +0530

    Update ppc64le-docker-build-test.sh

--- scripts/ppc64le-docker-build-test.sh
+++ scripts/ppc64le-docker-build-test.sh
@@ -7,5 +7,5 @@ trap remove_docker_container EXIT
 remove_docker_container
 
 docker run --network host --privileged -t -d --name milvus-build registry.access.redhat.com/ubi9/ubi:9.3
-docker cp ./. sumit:/
+docker cp ./. milvus-build:/
 docker exec -it milvus-build bash -c "chmod 777 scripts/ppc64le-build-ubi9.3.sh && ./scripts/ppc64le-build-ubi9.3.sh"
